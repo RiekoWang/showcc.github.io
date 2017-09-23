c9d067c3743232b66d3f03b51d8203e14692641d
showcc <showcccc_wlz@163.com>
2017-09-23 21:07:57 +0800

    Site updated: 2017-09-23 21:07:56

--- photos/photos.js
+++ photos/photos.js
@@ -1,9 +1,19 @@
 var render = function(page, data){
+
 	var img = "";
 	for (var i = 0; i < data.length; i++) {
-		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img class="lazy" data-original="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
+		img += '<li><div class="img-box">' + 
+		'<a class="img-bg " rel="external" data-fancybox="images" href="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img class="lazy" data-original="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '" src="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
 	}
 	$(".img-box-ul").append(img);
 	$(".img-box-ul").lazyload();
 	$("a[rel=example_group]").fancybox();
-}
+
+
+
+};
+
+$(function(){
+
+})
+	
