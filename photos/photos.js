436bd949dcaa7682b1c2481738ca30f16bab15e6
showcc <showcccc_wlz@163.com>
2017-09-23 19:38:19 +0800

    Site updated: 2017-09-23 19:38:18

--- photos/photos.js
+++ photos/photos.js
@@ -1,7 +1,7 @@
 var render = function(page, data){
 	var img = "";
 	for (var i = 0; i < data.length; i++) {
-		img += '<li><div class="img-box">' + '<a class="img-bg " rel="example_group" data-fancybox="images" href="ow4d8yam8.bkt.clouddn.com/' + data[i] + '"></a>' + '<img class="lazy" data-original="ow4d8yam8.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
+		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img class="lazy" data-original="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
 	}
 	$(".img-box-ul").append(img);
 	$(".img-box-ul").lazyload();
