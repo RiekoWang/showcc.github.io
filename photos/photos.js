66ad1131d8cb9a3f6867baf1740394f09c984e13
showcc <showcccc_wlz@163.com>
2017-10-15 01:44:29 +0800

    Site updated: 2017-10-15 01:44:22

--- photos/photos.js
+++ photos/photos.js
@@ -1,13 +1,16 @@
-var render = function(page, data){
+var render1 = function(page, data){
 
 	var img = "";
-	for (var i = 0; i < data.length; i++) {
-		img += '<li><div class="img-box">' + 
-		'<a class="img-bg " rel="external" data-fancybox="images" href="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img class="lazy" data-original="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '" src="http://ow4d8yam8.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
-	}
-	$(".img-box-ul").append(img);
+	var img1 = "";
+		img = '<li class="img-box1"><div class="img-box">' + 
+		'<a class="img-bg" href="./me" rel="example_group"></a>' + '<img class="lazy" src="http://ow4d8yam8.bkt.clouddn.com/296483-106.jpg">我的相册'  + '</div></li>';
+
+	img1 = '<li class="img-box2"><div class="img-box">' + 
+		'<a class="img-bg" href="./mylove" rel="example_group1"></a>' + '<img class="lazy" src="http://ow4d8yam8.bkt.clouddn.com/tattoo_by_wlop-da3qao4.jpg">我的收藏'  + '</div></li>';
+
+	$(".img-box-ul").append(img).append(img1);
 	$(".img-box-ul").lazyload();
-	$("a[rel=example_group]").fancybox();
+	$("a[rel=example_group1],a[rel=example_group]").fancybox();
 };
 
 	
